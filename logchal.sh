9b86313109bc04e5cdf1d3c1a2b51a0ba05b9e1d
Sean Minnick <sean.minnick11@gmail.com>
2025-01-17 14:54:55 -0500

    Add files via upload

---
+++ logchal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir httplogchal
+cd httplogchal
+wget https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/README.txt
+wget https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/httplog.txt
