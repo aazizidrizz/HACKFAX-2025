222a686da3433ea6b1efc4b9c8e43253571eb752
Sean Minnick <sean.minnick11@gmail.com>
2025-01-21 08:28:05 -0500

    Update logchal.sh

--- logchal.sh
+++ logchal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir httplogchal
-cd httplogchal
+mkdir /tmp/ctf_challenge/httplogchal
+cd /tmp/ctf_challenge/httplogchal
 wget https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/README.txt
 wget https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/httplog.txt
