161d8d71ca103d35368a3eec616e1cd85755248d
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-01-28 19:36:18 -0500

    Update and rename script2.sh to LinuxCHV2.sh

--- challenge2/LinuxCHV2.sh
+++ challenge2/LinuxCHV2.sh
@@ -3,6 +3,6 @@
 mkdir -p /tmp/ctf_challenge/level1
 cd /tmp/ctf_challenge/level1
 
-touch file1.txt file2.txt file3.txt
+touch file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt
 
 echo "CTF{Hydra_Intel}" > .hiddenfile.txt
