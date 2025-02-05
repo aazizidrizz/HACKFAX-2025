02ee514740e9871576bb5b377fadf840c86c2b7d
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-02-04 19:37:34 -0500

    Update LinuxCHV3.sh

--- challenge3/LinuxCHV3.sh
+++ challenge3/LinuxCHV3.sh
@@ -18,3 +18,6 @@ rm flag.txt layer1.zip layer2.zip
 
 # Set appropriate permissions for the files
 chmod 644 layer3.zip 
+
+# Set permissions for all files in the directory
+chmod 644 README.txt layer3.zip
