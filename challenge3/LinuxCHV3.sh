f5a65d2c9b0ae10f7d9ce627e9121927c6e4dae4
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-02-04 19:36:30 -0500

    Update LinuxCHV3.sh

--- challenge3/LinuxCHV3.sh
+++ challenge3/LinuxCHV3.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# Challenge: Decrypt the multi-layered archive and retrieve the flag.
 
+# Create challenge directory
+mkdir -p /tmp/ctf_challenge/level2
+cd /tmp/ctf_challenge/level2
+
+# Create a text file with the flag
+echo "CTF{Arc_Reactor_Design}" > flag.txt
+
+# Create multiple layers of archives without passwords
+zip layer1.zip flag.txt
+zip layer2.zip layer1.zip
+zip layer3.zip layer2.zip
+
+# Remove intermediate files
+rm flag.txt layer1.zip layer2.zip
+
+# Set appropriate permissions for the files
+chmod 644 layer3.zip 
