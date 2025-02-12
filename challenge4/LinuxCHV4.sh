9416c06ad38cb059b309594412d1118d3d0384e9
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-02-11 20:22:44 -0500

    Update LinuxCHV4.sh

--- challenge4/LinuxCHV4.sh
+++ challenge4/LinuxCHV4.sh
@@ -2,21 +2,21 @@
 
 # Challenge: Capture the flag from intercepted network traffic.
 
-# Create challenge directory
+# Create challenge directory if it doesn't exist
 mkdir -p /tmp/ctf_challenge/level4
 cd /tmp/ctf_challenge/level4
 
-# Start tcpdump in the background and capture packets
-tcpdump -i lo -w captured_traffic.pcap port 8080 &
+# Start tcpdump in the background to capture only packets on port 8080 and save to pcap
+sudo tcpdump -i lo -w captured_traffic.pcap port 8080 &
 
-# Allow tcpdump to start properly
-sleep 3
+# Allow tcpdump to start properly and begin capturing
+sleep 2
 
-# Simulate network transmission of the flag using Netcat (send only once)
-echo "CTF{Wakanda_Forever}" | nc -l -p 8080 -q 1 &
+# Simulate network transmission of the flag using Netcat (send once and stop after flag is transmitted)
+echo "CTF{Wakanda_Forever}" | nc -l -p 8080 -q 1
 
-# Allow time for transmission (use -q 1 to make sure netcat finishes transmission)
-sleep 3
+# Allow time for the flag to be transmitted before stopping capture
+sleep 2
 
-# Stop the tcpdump capture
+# Stop tcpdump to complete the capture and save the pcap file
 pkill tcpdump
