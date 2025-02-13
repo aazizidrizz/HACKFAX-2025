9ec488cd945d0d2eef8f635d67d59b345376580e
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-02-12 22:36:49 -0500

    Update LinuxCHV3.sh

--- challenge3/LinuxCHV3.sh
+++ challenge3/LinuxCHV3.sh
@@ -4,8 +4,8 @@
 echo "Setting up CTF challenge..."
 
 # Create challenge directory
-mkdir -p /tmp/ctf_challenge/level2 || { echo "Failed to create directory"; exit 1; }
-cd /tmp/ctf_challenge/level2 || { echo "Failed to access directory"; exit 1; }
+mkdir -p /tmp/ctf_challenge/level3 || { echo "Failed to create directory"; exit 1; }
+cd /tmp/ctf_challenge/level3 || { echo "Failed to access directory"; exit 1; }
 
 # Create a text file with the flag
 echo "CTF{Arc_Reactor_Design}" > flag.txt
