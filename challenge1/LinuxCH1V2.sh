c56fe8fa732b64516062ee59d577e09d47ede431
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-03-06 09:59:00 -0500

    Create LinuxCH1V2.sh

---
+++ challenge1/LinuxCH1V2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p /tmp/ctf_challenge/httplogchal
+cd /tmp/ctf_challenge/httplogchal || { echo "Error: Could not change directory."; exit 1; }
+wget https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/README.txt || { echo "Error: Failed to download README.txt"; exit 1; }
+wget https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/httplog.txt || { echo "Error: Failed to download httplog.txt"; exit 1; }
+echo "Downloads completed successfully."
