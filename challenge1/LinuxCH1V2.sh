3c32ee8c286260bfa4b9dd2affa3b19a87677644
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-03-06 10:40:17 -0500

    Delete challenge1/LinuxCH1V2.sh

--- challenge1/LinuxCH1V2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p /tmp/ctf_challenge/httplogchal
-cd /tmp/ctf_challenge/httplogchal || { echo "Error: Could not change directory."; exit 1; }
-wget "https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/httplog.txt" || { echo "Error: Failed to download httplog.txt"; exit 1; }
-echo "Downloads completed successfully."
