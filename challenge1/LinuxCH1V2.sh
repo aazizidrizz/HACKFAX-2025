803be8c6d47f2b4dbe898de3f3c3ad69c4cfba56
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-03-09 13:28:30 -0400

    Create LinuxCH1V2.sh

---
+++ challenge1/LinuxCH1V2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p /tmp/ctf_challenge/httplogchal
+cd /tmp/ctf_challenge/httplogchal
+curl -O https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/httplog.txt
