ab8b0b3f01282bf602541eae57055628feaf9262
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-03-09 13:41:21 -0400

    Update LinuxCH1V2.sh

--- challenge1/LinuxCH1V2.sh
+++ challenge1/LinuxCH1V2.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /tmp/ctf_challenge/httplogchal
 cd /tmp/ctf_challenge/httplogchal
-curl -O https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/httplog.txt
+curl -O https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/challenge1/httplog.txt
