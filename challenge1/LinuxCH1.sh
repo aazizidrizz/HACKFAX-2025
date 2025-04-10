d04932649a2125773c67545da1b197366fbe07fb
aazizidrizz <ahmedabdelaziz661225@gmail.com>
2025-04-10 14:55:36 -0400

    Delete challenge1/LinuxCH1.sh

--- challenge1/LinuxCH1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p /tmp/ctf_challenge/level1
-cd /tmp/ctf_challenge/level1
-curl -O https://raw.githubusercontent.com/SeanMinnick/HackFax-2025/refs/heads/main/challenge1/httplog.txt
